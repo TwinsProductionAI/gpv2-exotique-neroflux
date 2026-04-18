db08fd18e390179b66885f6bc1879d6b70754109
Xavier <xfleriag@gmail.com>
2026-04-17 22:40:18 -0400

    feat: expose Aletheia protocol API

--- src/gpv2_neroflux/__init__.py
+++ src/gpv2_neroflux/__init__.py
@@ -1,5 +1,11 @@
-﻿"""GPV2_EXOTIQUE_NEROFLUX public API."""
+"""GPV2 Exotique public API."""
 
+from .aletheia import AletheiaProtocol, normalize_post_maj_packet
 from .neroflux import NerofluxRouter, normalize_packet
 
-__all__ = ["NerofluxRouter", "normalize_packet"]
+__all__ = [
+    "AletheiaProtocol",
+    "NerofluxRouter",
+    "normalize_packet",
+    "normalize_post_maj_packet",
+]
